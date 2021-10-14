21896d4d527ce622fda743431c432a6510e0d3e8
Kiryl Krasouski <ziksow@gmail.com>
2021-10-14 21:43:13 +0300

    fix: 1) default argument of function added for case with no arg 2) added case with empty string and string including non-alphabetic charecters 3) fix return by join with dot

--- hw4-js/task2.js
+++ hw4-js/task2.js
@@ -1,19 +1,27 @@
 let input = "Hippopotomonstrosesquippedaliophobia";
 
-function lettersToNumbers(input) {
-  let inputString = input.toLowerCase();
-  let arrFromString = inputString.split("");
-  let clearArr = [];
+function lettersToNumbers(input = undefined) {
+  if (input === undefined) {
+    return "Error! Empty argument";
+  } else if (input.length === 0) {
+    return "Input string is empty!";
+  } else if (!/^[a-zа-я]+$/i.test(input)) {
+    return "Only alphbetic, pls!";
+  } else {
+    let inputString = input.toLowerCase();
+    let arrFromString = inputString.split("");
+    let clearArr = [];
 
-  let outputArr = arrFromString.map((item, i) => {
-    if (arrFromString.indexOf(item) == i) {
-      clearArr.push(item);
-      return clearArr.indexOf(item);
-    } else {
-      return clearArr.indexOf(item);
-    }
-  });
-  return outputArr;
+    let outputArr = arrFromString.map((item, i) => {
+      if (arrFromString.indexOf(item) == i) {
+        clearArr.push(item);
+        return clearArr.indexOf(item);
+      } else {
+        return clearArr.indexOf(item);
+      }
+    });
+    return outputArr.join(".");
+  }
 }
 
 console.log(lettersToNumbers(input));
