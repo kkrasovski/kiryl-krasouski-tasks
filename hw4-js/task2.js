ff06717a0ca085614e2e10c81e0f6cbbb91aa829
Kiryl Krasouski <ziksow@gmail.com>
2021-10-14 22:49:31 +0300

    fix: add description for sub task

--- hw4-js/task2.js
+++ hw4-js/task2.js
@@ -1,5 +1,5 @@
 let input = "Hippopotomonstrosesquippedaliophobia";
-
+//let input = "Hello";
 function lettersToNumbers(input = undefined) {
   if (input === undefined) {
     return "Error! Empty argument";
@@ -8,16 +8,14 @@ function lettersToNumbers(input = undefined) {
   } else if (!/^[a-zа-я]+$/i.test(input)) {
     return "Only alphbetic, pls!";
   } else {
-    let inputString = input.toLowerCase();
-    let arrFromString = inputString.split("");
+    let arrFromString = input.toLowerCase().split("");
     let clearArr = [];
-
     let outputArr = arrFromString.map((item, i) => {
       if (arrFromString.indexOf(item) == i) {
         clearArr.push(item);
         return clearArr.indexOf(item);
       } else {
-        return clearArr.indexOf(item);
+        return clearArr.indexOf(item); 
       }
     });
     return outputArr.join(".");
@@ -25,3 +23,21 @@ function lettersToNumbers(input = undefined) {
 }
 
 console.log(lettersToNumbers(input));
+
+// function lettersToNumbers(input) {
+
+//     let arrFromString = input.toLowerCase().split("");   
+//     console.log(arrFromString)
+//     let outputArr = arrFromString.map((item,i, array) => {   
+      
+//       if (arrFromString.indexOf(item) != i) {
+//         return arrFromString.indexOf(item)
+//       } else {
+       
+//        return arrFromString.indexOf(item)
+//       }
+// })
+// return outputArr
+// }
+
+// console.log(lettersToNumbers(input));
