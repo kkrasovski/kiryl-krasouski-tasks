afdec01a4267e8afb2c2b8b6658cf5036ee1017b
Kiryl Krasouski <ziksow@gmail.com>
2021-10-27 01:05:01 +0300

    feat: added functionality for selecting the first day

--- js-hw5-6-calendarJS/assets/js/calendar.js
+++ js-hw5-6-calendarJS/assets/js/calendar.js
@@ -1,10 +1,31 @@
 const date = new Date();
-const renderCalendar = () => {
-  date.setDate(1);
+date.setDate(1);
+const daysLabelsMon = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+const daysLabelsSun = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+const monthLabel = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 
-  const calendarGrid = document.querySelector('.calendar__grid');
-  
-  
+const renderCalendar = () => {
+  const calendarGrid = document.querySelector(".calendar__grid");
+
+  const lastDayIndex = new Date(
+    date.getFullYear(),
+    date.getMonth() + 1,
+    0
+  ).getDay();
+
   const lastDay = new Date(
     date.getFullYear(),
     date.getMonth() + 1,
@@ -17,78 +38,103 @@ const renderCalendar = () => {
     0
   ).getDate();
 
-  const firstDayIndex = date.getDay()-1;
-console.log(firstDayIndex)
+  let firstDayIndex = date.getDay();
 
-  const lastDayIndex = new Date(
-    date.getFullYear(),
-    date.getMonth() + 1,
-    0
-  ).getDay();
+  let nextDays = 7 - lastDayIndex - 1; 
 
-  //const nextDays = 7 - lastDayIndex - 1;
-  const nextDays = 7 - lastDayIndex ;
-  let dayLabel = [
-    "Mon",
-    "Tue",
-    "Wed",
-    "Thu",
-    "Fri",
-    "Sat",
-    "Sun"];
-  
-  let monthLabel = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
-  ];
-  
-  document.querySelector('.season__handler').innerHTML = monthLabel[date.getMonth()];
+  if (localStorage.firstWeekDay == "mon" || localStorage.firstWeekDay == undefined) {
+    firstDayIndex = date.getDay() - 1;
+    if (date.getDay() == 0) {
+      firstDayIndex = date.getDay() - 1 + 7;
+    }
+
+    if (lastDayIndex == 0) {
+      nextDays = 7;
+    } else {
+      nextDays = 7 - lastDayIndex;
+    }
+  }
+
+
+
+  document.querySelector(".season__handler").innerHTML =
+    monthLabel[date.getMonth()];
+
   let days = "";
-  console.log(firstDayIndex)
-  console.log(prevLastDay)
 
   for (let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="calendar__day day_prev">${prevLastDay - x +1}</div>`;
-    console.log('1')
+    days += `<div class="calendar__day day_prev">${prevLastDay - x + 1}</div>`;
   }
-  
-  for (let i=1;  i<=lastDay; i++) {
-    console.log('2')
-    if (i === new Date().getDate() && date.getMonth() === new Date().getMonth()) {
+
+  for (let i = 1; i <= lastDay; i++) {
+    if (
+      i === new Date().getDate() &&
+      date.getMonth() === new Date().getMonth()
+    ) {
       days += `<div class="calendar__day day_current">${i}</div>`;
     } else {
       days += `<div class="calendar__day day">${i}</div>`;
     }
-    
-    
   }
-  
-  for (let j=1; j<=nextDays; j++) {
-    console.log('3')
-    days+=`<div class="calendar__day day_next">${j}</div>`;
+
+  for (let j = 1; j <= nextDays; j++) {
+    days += `<div class="calendar__day day_next">${j}</div>`;
     calendarGrid.innerHTML = days;
+  }
+};
+renderCalendar();
+
+
+// draw names of the day
+function setFirstDay() {  
+  const daysOfWeek = document.querySelector(".calendar__days");
+  let daysNames = "";
+  if (localStorage.firstWeekDay == "sun") {
+    for (let k = 0; k < 7; k++) {
+      daysNames += `<div class="calendar__day-of-week" href="#">${daysLabelsSun[k]}</div>`;
+      daysOfWeek.innerHTML = daysNames;
+    }
+  } else {
+    for (let k = 0; k < 7; k++) {
+      daysNames += `<div class="calendar__day-of-week" href="#">${daysLabelsMon[k]}</div>`;
+      daysOfWeek.innerHTML = daysNames;
+    }
   }
 }
 
+document.querySelector(".arrow_prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+  console.log(date);
+  renderCalendar();
+});
 
-document.querySelector('.arrow_prev').addEventListener('click', () => {
-    date.setMonth(date.getMonth() - 1);
-    renderCalendar()
-  })
+document.querySelector(".arrow_next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  renderCalendar();
+  console.log(date);
+});
+
+// select first day in settings
+
+let firstWeekDayHandler = document.querySelector('[name="first-day"]');
+
+function SelectFirstDay() {
+  let firstWeekDay =
+    firstWeekDayHandler.options[firstWeekDayHandler.selectedIndex].value;
+  localStorage.setItem("firstWeekDay", firstWeekDay);
+  console.log(localStorage);
+  setFirstDay();
+}
 
 
-document.querySelector('.arrow_next').addEventListener('click', () => {
-    date.setMonth(date.getMonth() + 1)
-    renderCalendar()
-  })
-  renderCalendar()
+
+
+let saveSettings = document.querySelector("#save");
+
+saveSettings.addEventListener("click", () => {
+  SelectFirstDay();
+  popUpHandler();
+  renderCalendar();
+});
+
+
