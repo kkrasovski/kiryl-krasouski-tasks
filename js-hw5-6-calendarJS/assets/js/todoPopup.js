31d6bcdbb37b84fde23aa64f2df1bf7ef69540d8
Kiryl Krasouski <ziksow@gmail.com>
2021-10-28 19:27:15 +0300

    fix: fixed some bugs

--- js-hw5-6-calendarJS/assets/js/todoPopup.js
+++ js-hw5-6-calendarJS/assets/js/todoPopup.js
@@ -1,8 +1,12 @@
-function todoOpen() {
+function todoOpen(e) {  
   let todoPopUpWrapper = document.querySelector(".container__todo");
   let closeBtnTodo = document.querySelector(".pop-up__close-btn_todo");
+  let dateForTodo = document.querySelector(".pop-up__nunber");
+  console.log(e.target.innerText)  
+  dateForTodo.innerText = e.target.innerText;
+
   closeBtnTodo.addEventListener("click", () => {
-    todoOpen();
+    todoOpen(e);
   });
   todoPopUpWrapper.classList.toggle("container__todo_close");
 }
