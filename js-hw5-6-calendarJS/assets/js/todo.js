a25007bc6d2576367eaa34e2255394bef0838b01
Kiryl Krasouski <ziksow@gmail.com>
2021-10-28 12:15:20 +0300

    feat: add test object for saving data

--- js-hw5-6-calendarJS/assets/js/todo.js
+++ js-hw5-6-calendarJS/assets/js/todo.js
@@ -1,45 +1,47 @@
-
-
-let todoAddBtn = document.querySelector('.todo__add-btn');
-let todoInput = document.querySelector('.todo__input_text');
-let todoWrapper = document.querySelector('.todo__wrapper');
-let delBtn = document.querySelector('.todo-task__del');
-let totalTasks = document.querySelector('.todo__total');
+let todoAddBtn = document.querySelector(".todo__add-btn");
+let todoInput = document.querySelector(".todo__input_text");
+let todoWrapper = document.querySelector(".todo__wrapper");
+let delBtn = document.querySelector(".todo-task__del");
+let totalTasks = document.querySelector(".todo__counter");
+console.log(totalTasks);
 let counter = 0;
-todoAddBtn.addEventListener('click', (e) => {  
-  addInList(todoInput.value)  
-  todoInput.value = '';
- e.preventDefault()
-})
+let todoList = {};
+todoAddBtn.addEventListener("click", (e) => {
+  addInList(todoInput.value);
+  todoInput.value = "";
+  e.preventDefault();
+});
 
-function addInList(value) {   
+function addInList(value) {
   counter++;
-
-const li = document.createElement('li');
-li.className = 'todo__task todo-task';
-li.textContent = value;
-
-const btn = document.createElement('li')
-btn.className = 'todo-task__del';
-li.appendChild(btn);
-
-
-btn.addEventListener('click', (e) => {  
-  todoWrapper.removeChild(li);
-  counter--;
-  totalTasks.textContent = counter;
-})
-
-li.addEventListener('click', (e) => {
-  li.classList.toggle('todo-task_done');
-
-
+  todoList[counter] = value;
  
-})
-totalTasks.textContent = counter
-todoWrapper.appendChild(li);
+  console.log(todoList);
+
+  todoWrapper.innerHTML = "";
+  for (key in todoList) {
+    console.log(key);
+    const li = document.createElement("li");
+    li.className = "todo__task todo-task";
+    li.textContent = todoList[key];
+    const btn = document.createElement("div");
+    btn.className = "todo-task__del";
+    li.appendChild(btn);
+
+    btn.addEventListener("click", (e) => {
+      console.log(e)
+      console.log(this)
+      console.log(counter)
+      todoWrapper.removeChild(li);
+      counter--;
+      totalTasks.textContent = counter;
+      todoList[counter];
+    });
+
+    li.addEventListener("click", (e) => {
+      li.classList.toggle("todo-task_done");
+    });
+    totalTasks.textContent = counter;
+    todoWrapper.appendChild(li);
+  }
 }
-
-
-
-  
