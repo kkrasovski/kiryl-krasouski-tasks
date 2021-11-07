46f7f2cc820f97d9d8f00d0b6f4e2cf1782fbe5a
Kiryl Krasouski <ziksow@gmail.com>
2021-11-07 20:44:35 +0300

    feat: split into multiple files js and css

--- js-hw5-6-calendarJS/assets/js/calendar.js
+++ js-hw5-6-calendarJS/assets/js/calendar.js
@@ -3,6 +3,18 @@ date.setDate(1);
 
 const holidayConfig = ["2022.01.14", "2022.01.4", "2021.12.02", "2021.11.30"];
 
+document.querySelector(".arrow_prev").addEventListener("click", () => {
+  date.setMonth(date.getMonth() - 1);
+
+  calendarGrid.innerHTML = "";
+  renderCalendar();
+});
+
+document.querySelector(".arrow_next").addEventListener("click", () => {
+  date.setMonth(date.getMonth() + 1);
+  calendarGrid.innerHTML = "";
+  renderCalendar();
+});
 
 if (localStorage.getItem("showAdditionalDays") == null) {
   localStorage.setItem("showAdditionalDays", true);
@@ -232,108 +244,12 @@ function setFirstDay() {
     }
   }
 }
-
-document.querySelector(".arrow_prev").addEventListener("click", () => {
-  date.setMonth(date.getMonth() - 1);
-
-  calendarGrid.innerHTML = "";
-  renderCalendar();
-});
-
-document.querySelector(".arrow_next").addEventListener("click", () => {
-  date.setMonth(date.getMonth() + 1);
-  calendarGrid.innerHTML = "";
-  renderCalendar();
-});
-
+// draw weekends
 function setWeekends(weekendDays, g) {
   if (
     weekendDays.getDay() == localStorage.firstWeekendDay ||
     weekendDays.getDay() == localStorage.secondWeekendDay
   ) {
     g.classList.add("day_weekend");
-  }
-}
-
-// SETTINGS
-
-let firstWeekDayHandler = document.querySelector('[name="first-day"]');
-
-// settings for first day of the week
-function selectFirstDay() {
-  let firstWeekDay =
-    firstWeekDayHandler.options[firstWeekDayHandler.selectedIndex].value;
-  localStorage.setItem("firstWeekDay", firstWeekDay);
-  setFirstDay();
-}
-
-// ON/OF ToDo list
-document
-  .querySelector(".pop-up__checkbox-item")
-  .addEventListener("change", todoActivation);
-
-function todoActivation() {
-  if (!addTodo.checked) {
-    localStorage.setItem("todoActive", false);
-  } else {
-    localStorage.setItem("todoActive", true);
-  }
-}
-
-// Save Btn
-const saveSettings = document.querySelector("#save");
-saveSettings.addEventListener("click", () => {
-  selectFirstDay();
-  popUpHandler();
-  selectWeekends();
-  calendarGrid.innerHTML = "";
-  showMonth();
-  renderCalendar();
-});
-
-// default weekend setting 
-
-const firstWeekend = document.getElementById("first-weekend");
-const secondWeekend = document.getElementById("second-weekend");
-
-for (let i = 0; i < firstWeekend.options.length; i++) {
-  let firstDefaultWeekend = firstWeekend.options[i];
-  if (firstDefaultWeekend.value == localStorage.getItem("firstWeekendDay")) {
-    firstDefaultWeekend.setAttribute("selected", true);
-  }
-}
-
-for (let i = 0; i < secondWeekend.options.length; i++) {
-  let secondDefaultWeekend = secondWeekend.options[i];
-  if (secondDefaultWeekend.value == localStorage.getItem("secondWeekendDay")) {
-    secondDefaultWeekend.setAttribute("selected", true);
-  }
-}
-
-function selectWeekends() {
-  let firstWeekendDay = firstWeekend.options[firstWeekend.selectedIndex].value;
-  let secondWeekendDay =
-    secondWeekend.options[secondWeekend.selectedIndex].value;
-  localStorage.setItem("firstWeekendDay", firstWeekendDay);
-  localStorage.setItem("secondWeekendDay", secondWeekendDay);
-}
-
-// settings for days of the next and previous month
-const showRearMonth = document.getElementById("show-month");
-const hideRearMonth = document.getElementById("hide-month");
-
-if (localStorage.getItem("showAdditionalDays") == "true") {
-  showRearMonth.checked = true;
-} else {
-  hideRearMonth.checked = true;
-}
-
-function showMonth() {
-  if (showRearMonth.checked) {
-    localStorage.setItem("showAdditionalDays", true);
-  }
-
-  if (hideRearMonth.checked) {
-    localStorage.setItem("showAdditionalDays", false);
   }
 }
